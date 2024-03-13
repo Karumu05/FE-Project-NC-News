71e5dc95b70e843c918f81a7220d0ac8550385df
Calum <calum.borders@gmail.com>
2024-03-13 13:14:54 +0000

    Started implimenting user features and added the functionality to delete a comment from an article

--- src/Components/IndividualComment.jsx
+++ src/Components/IndividualComment.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import UserContext from "../Contexts/User";
+import { deleteCommentFromArticle } from "../api";
 
-const IndividualComment = ({ comment }) => {
-
+const IndividualComment = ({ comment, setDeletedComment, deletedComment }) => {
   const timeReg = /(\d{2}:\d{2})/g;
   const displayTime = comment.created_at.match(timeReg);
+  const user = useContext(UserContext);
+  const [error, setError] = useState(false);
+
+  function handleDelete() {
+    deleteCommentFromArticle(comment.comment_id)
+      .then(() => {
+        setDeletedComment(true);
+        setError(false);
+        setComments([...comments]);
+      })
+      .catch(() => {
+        setError(true);
+      });
+    setDeletedComment(false);
+  }
 
   return (
     <div className="flex items-center gap-2.5">
@@ -15,15 +31,35 @@ const IndividualComment = ({ comment }) => {
           <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
             {displayTime}
           </span>
+
+          {user.username === comment.author ? (
+            <span>
+              <button onClick={handleDelete}>
+                <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
+                  <svg
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
+                  </svg>
+                  <span className="sr-only">Delete button</span>
+                </div>
+              </button>
+            </span>
+          ) : null}
         </div>
+        {error ? alert("Comment not deleted, please try again") : null}
         <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">
           {comment.body}
         </p>
+
         <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
           Votes: {comment.votes}
         </span>
       </div>
-      <button></button>
     </div>
   );
 };
