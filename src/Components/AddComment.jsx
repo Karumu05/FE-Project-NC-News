699a8152f0a4fc911daa9ae4a3c796f3d96431e3
Calum <calum.borders@gmail.com>
2024-03-13 10:48:07 +0000

    added user error feedback for adding comments

--- src/Components/AddComment.jsx
+++ src/Components/AddComment.jsx
@@ -3,7 +3,8 @@ import { postNewCommentToArticle } from "../api";
 
 const AddComment = ({ article_id, comments, setComments }) => {
   const [newComment, setNewComment] = useState("");
-  const [postedComment, setPostedComment] = useState(false)
+  const [postedComment, setPostedComment] = useState(false);
+  const [error, setError] = useState(false);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -11,16 +12,19 @@ const AddComment = ({ article_id, comments, setComments }) => {
       username: "grumpy19", //this will be amended to the user once the functionality has been added
       body: newComment,
     };
-    postNewCommentToArticle(article_id, commentToPost).then(
-      (newCommentData) => {
-        setPostedComment(true)
+    postNewCommentToArticle(article_id, commentToPost)
+      .then((newCommentData) => {
+        setPostedComment(true);
+        setError(false);
         setComments([...comments, newCommentData]);
         setNewComment("");
         setTimeout(() => {
-          setPostedComment(false)
+          setPostedComment(false);
         }, 2000);
-      }
-    );
+      })
+      .catch((error) => {
+        setError(true);
+      });
   }
 
   return (
@@ -48,6 +52,31 @@ const AddComment = ({ article_id, comments, setComments }) => {
             >
               {postedComment ? "Comment Added" : "Post Comment"}
             </button>
+            {error ? (
+              <div className="ml-10">
+                <div
+                  id="toast-danger"
+                  className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+                  role="alert"
+                >
+                  <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
+                    <svg
+                      className="w-5 h-5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
+                    </svg>
+                    <span className="sr-only">Error icon</span>
+                  </div>
+                  <div className="ms-3 text-sm  font-normal">
+                    Something went wrong, please try again
+                  </div>
+                </div>
+              </div>
+            ) : null}
           </div>
         </div>
       </form>
