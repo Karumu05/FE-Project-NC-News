d439735d6a0094ed5bde75bb3636dcd4d9da0d00
Calum <calum.borders@gmail.com>
2024-03-14 16:24:19 +0000

    Added the functionality to search by query via articles

--- src/Components/ArticleList.jsx
+++ src/Components/ArticleList.jsx
@@ -1,21 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { fetchArticles } from "../api";
 import ArticleCard from "./ArticleCard";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
+import Dropdown from "./Dropdown";
+import SortByDropdown from "./SortByDropdown";
 
 const ArticleList = () => {
   const [articles, setArticles] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const { topic } = useParams();
 
-  const {topic} = useParams()
+  const [searchQuery, setSearchQuery] = useState({});
+
+  const sortByItems = [
+    { value: "created_at", label: "Date" },
+    { value: "comment_count", label: "Comment count" },
+    { value: "votes", label: "votes" },
+  ];
+
+  const ascDesc = [
+    { value: "asc", label: "Acending" },
+    { value: "desc", label: "Decending" },
+  ];
 
   useEffect(() => {
     setIsLoading(true);
-    fetchArticles(topic).then((result) => {
-      setArticles(result);
-      setIsLoading(false);
-    });
-  }, [topic]);
+    fetchArticles(topic, searchQuery.sort_by, searchQuery.order).then(
+      (result) => {
+        setArticles(result);
+        setIsLoading(false);
+      }
+    );
+  }, [topic, searchQuery]);
 
   return isLoading ? (
     <div key="loading-state">
@@ -23,11 +39,23 @@ const ArticleList = () => {
     </div>
   ) : (
     <>
-    <div  className="card h-full justify-center items-center flex-col">
-      {articles.map((article) => {
-        return <ArticleCard key={article.article_id} article={article} />;
-      })}
-    </div>
+      <div className="flex flex-row justify-evenly items-center bg-white border border-gray-200 rounded-lg shadow border-gray-700 dark:bg-gray-800  m-2 p-2">
+        <SortByDropdown
+          label="Sort Articles By ..."
+          sortByItems={sortByItems}
+          setSearchQuery={setSearchQuery}
+        />
+        <Dropdown
+          label="Sort Order By ..."
+          ascDesc={ascDesc}
+          setSearchQuery={setSearchQuery}
+        />
+      </div>
+      <div className="card h-full justify-center items-center flex-col">
+        {articles.map((article) => {
+          return <ArticleCard key={article.article_id} article={article} />;
+        })}
+      </div>
     </>
   );
 };
